14f6bad837057e0d0e024f8d2152c253c6a8f391
AbhishekChimavekar <abhishek@krinati.co>
2026-02-27 22:42:47 +0530

    Refractor to ViewSets (in progress)

--- library/views.py
+++ library/views.py
@@ -1,59 +1,15 @@
-from rest_framework import generics
-from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
-from .models import Book, Loan
-from .serializers import BookSerializer, LoanSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from models import Book, Loan
+from serializers import BookSerializer, LoanSerializer
 
-
-
-class BookListCreateView(generics.ListCreateAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by("title")
     serializer_class = BookSerializer
-
-class AvailableBookListView(generics.ListAPIView):
-    queryset = Book.objects.filter(available_copies__gt=0).order_by("title")
-    serializer_class = BookSerializer
-
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class LoanListCreateView(generics.ListCreateAPIView):
-    queryset = Loan.objects.all().order_by("-start_date")
-    serializer_class = LoanSerializer
-
-    def perform_create(self, serializer):
-        book = serializer.validated_data["book"]
-
-        if book.available_copies <= 0:
-            # Stop creation if no stock
-            raise ValidationError("No available copies for this book.")
-
-        # Decrement stock and save both Book and Loan
-        book.available_copies -= 1
-        book.save()
-        serializer.save()
-
     
-class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Loan.objects.all()
-    serializer_class = LoanSerializer
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop("partial", False)
-        instance = self.get_object()
-        was_returned = instance.returned  # state before update
-
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        updated_loan = serializer.save()
-
-        # If it was not returned before but now is returned, add one copy back
-        if not was_returned and updated_loan.returned:
-            book = updated_loan.book
-            book.available_copies += 1
-            book.save()
-
+    @action(detail=False, methods=["get"])
+    def available(self, request):
+        books = Book.objects.filter(available_copies__gt=0).order_by("title")
+        serializer = self.get_serializer(books, many=True)
         return Response(serializer.data)
-
+    
