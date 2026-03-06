58b420a10b492c3480e8332632a98180f9dc113c
AbhishekChimavekar <abhishek@krinati.co>
2026-03-06 20:26:07 +0530

    clean repo and apply gitignore

--- library/urls.py
+++ library/urls.py
@@ -1,16 +1,11 @@
-from django.urls import path
-from .views import (
-    BookListCreateView,
-    BookDetailView,
-    LoanListCreateView,
-    LoanDetailView,
-    AvailableBookListView
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, LoanViewSet
+
+router = DefaultRouter()
+router.register(r"books", BookViewSet)
+router.register(r"loans", LoanViewSet)
 
 urlpatterns = [
-    path("books/available/", AvailableBookListView.as_view(), name="available-book-list"),
-    path("books/", BookListCreateView.as_view(), name="book-list-create"),
-    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
-    path("loans/", LoanListCreateView.as_view(), name="loan-list-create"),
-    path("loans/<int:pk>/", LoanDetailView.as_view(), name="loan-detail"),
+    path("", include(router.urls)),
 ]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from models import Book, Loan
-from serializers import BookSerializer, LoanSerializer
+from .models import Book, Loan
+from .serializers import BookSerializer, LoanSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by("title")
@@ -17,4 +18,25 @@ class BookViewSet(viewsets.ModelViewSet):
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all().order_by("-start_date")
     serializer_class = LoanSerializer
+
+    def perform_create(self, serializer):
+        book = serializer.validated_data["book"]
+
+        if book.available_copies <= 0:
+            raise ValidationError("No available copies for this book.")
+
+        book.available_copies -= 1
+        book.save()
+        serializer.save()
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        was_returned = instance.returned
+        updated_loan = serializer.save()
+
+        if not was_returned and updated_loan.returned:
+            book = updated_loan.book
+            book.available_copies += 1
+            book.save()
+        
     
